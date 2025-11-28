3cd619b6548d678b2cbef89fb2e180788e5ad814
wz <wz@none.com>
2025-11-28 17:24:19 +0800

    增加toDeepRaw

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,5 +1,9 @@
 import { v7 } from 'uuid'
+import { cloneDeep } from 'es-toolkit'
 
 export const getUid = () => {
   return v7()
 }
+
+/**获取原生对象，避免与electron通信时报响应对象无法克隆的错误 */
+export const toDeepRaw: <T>(v: T) => T = (v) => cloneDeep(v)
